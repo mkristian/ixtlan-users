2db785491ecb958bc889f03317f979ab13dcdee8
Kristian Meier <m.kristian@web.de>
2011-09-06 19:21:32 +0530

    fix mail content-type

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,11 +49,13 @@ Users::Application.configure do
 
 end
 
-ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "25",
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['SENDGRID_DOMAIN']
-}
+if ENV['SENDGRID_USERNAME']
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+end
