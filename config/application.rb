c1fea2e391d0b9b5caddd87497f922266e18df34
Kristian Meier <m.kristian@web.de>
2011-10-30 13:02:41 +0530

    added deflater rack

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Users
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
   end
 end
