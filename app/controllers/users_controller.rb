b252dd6dce27b7ffa48140508e7c3ff47f55c349
Kristian Meier <m.kristian@web.de>
2011-11-05 15:53:53 +0530

    relative usermanagement on update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -127,14 +127,41 @@ class UsersController < ApplicationController
     # delete groups but keep group_ids
     params[:user].delete(:groups)
 
-    #TODO on guard side this should be part of the controller
+    #TODO allowed? should be part of guard
     unless guard.allowed?("users", "change", current_user_group_names)
       params[:user].delete(:login)
       params[:user].delete(:email)
     end
 
+    unless current_user.root?
+      # restrict group changes to current_user.groups
+      current_group_ids = current_user.groups.collect do |g|
+        g.id
+      end
+      # the ids from the request
+      requested_group_ids = params[:user].delete(:group_ids) || []
+      # intersection if request with current_user
+      new_group_ids = current_group_ids - (current_group_ids - requested_group_ids)
+      # groups of the user as in database
+      user_group_ids = @user.groups.collect do |g|
+        g.id
+      end
+      # the new set of groups
+      group_ids = user_group_ids - current_group_ids + new_group_ids
+   
+      # set the group_ids for the update_attributes method
+      params[:user][:group_ids] = group_ids
+    end
+
     respond_to do |format|
       if @user.update_attributes(params[:user])
+        
+        unless current_user.root?
+          @user.groups.delete_if do |g|
+            ! current_user.groups.member? g
+          end
+        end
+
         format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
         format.xml  { render :xml => @user.to_xml(User.single_options) }
         format.json  { render :json => @user.to_json(User.single_options) }
