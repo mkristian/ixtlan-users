3907890a1439ed485a060942990c13fcfbc247a6
Kristian Meier <m.kristian@web.de>
2011-11-17 11:58:20 +0530

    getting it to work with MRI

--- ixtlan-gwt.rb
+++ ixtlan-gwt.rb
@@ -1,3 +1,11 @@
 # ixtlan client template for rails applications with GWT frontend
 
+#TODO something with the rails version: Rails::VERSION::STRING
+
+gem 'resty-generators'
+
+run 'bundle install'
+
+generate 'resty:setup', ENV['GWT_MODULE'] || 'com.example', '--session', '--menu'
+
 apply 'http://mkristian.github.com/ixtlan-users/generate.rb'
--- ixtlan.rb
+++ ixtlan.rb
@@ -1,6 +1,6 @@
 # ixtlan client template for rails applications
 
-gem "ixtlan-core", '~> 0.7.0'
+gem "ixtlan-core", '~> 0.7.1'
 gem "ixtlan-guard", '~> 0.7.0'
 gem "ixtlan-session-timeout", '~> 0.4.0'
 
