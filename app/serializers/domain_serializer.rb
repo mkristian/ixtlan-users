102a0d62cfa326c7ecbecaf72cdcef06b3ae5b5d
Christian Meier <m.kristian@web.de>
2012-12-31 14:21:54 +0530

    reduce doublication

--- app/serializers/domain_serializer.rb
+++ app/serializers/domain_serializer.rb
@@ -1,23 +1,5 @@
-require 'ixtlan/babel/serializer'
-
-class DomainSerializer < Ixtlan::Babel::Serializer
+class DomainSerializer < RegionSerializer
 
   root 'domain'
 
-  add_context(:update,
-              :only => [:id, :name, :updated_at]
-             )
-
-  add_context(:collection,
-              :except => [:created_at, :modified_by_id]
-             )
-
-  add_context(:single,
-              :except => [:modified_by_id],
-              :include => {
-                :modified_by => {
-                  :only => [:id, :login, :name]
-                }
-              }
-             )
 end
