e7218ddd48284db8a8a5c0a826b3c5661ea9fe38
Christian Meier <m.kristian@web.de>
2012-12-31 19:44:44 +0530

    share setup between seralizers

---
+++ app/serializers/common_serializer.rb
@@ -0,0 +1,18 @@
+[ RegionSerializer, DomainSerializer ].each do |clazz|
+  clazz.add_context(:update,
+                    :only => [:id, :name, :updated_at]
+                    )
+
+  clazz.add_context(:collection,
+                    :except => [:created_at, :modified_by_id]
+                    )
+
+  clazz.add_context(:single,
+                    :except => [:modified_by_id],
+                    :include => {
+                      :modified_by => {
+                        :only => [:id, :login, :name]
+                      }
+                    }
+                    )
+end
