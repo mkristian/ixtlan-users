7ad3d208a1a15610beb3e4540da697dac9cf67e2
Christian Meier <m.kristian@web.de>
2013-01-14 00:47:51 +0530

    fixed const

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -1,7 +1,7 @@
 # the error dumps will be cleanup after the days to keeps dump expired
 
 Users::Application.config.error_dumper.block = Proc.new do |dumper|
-  config = Confguration.instance
+  config = Configuration.instance
   dumper.base_url = config.errors_base_url
   dumper.from_email = config.errors_from_email
   dumper.to_emails = config.errors_to_emails
