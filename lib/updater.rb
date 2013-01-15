a753f335b32f967a511a4c38f47b024ebf60d0f2
Christian Meier <m.kristian@web.de>
2013-01-15 14:43:13 +0530

    use right application config

--- lib/updater.rb
+++ lib/updater.rb
@@ -2,7 +2,7 @@ require 'ixtlan/remote/sync'
 class Updater < Ixtlan::Remote::Sync
 
   def initialize
-    super Users::Application.config.restserver
+    super Users::Application.config.rest
     register( Locale )
   end
 
