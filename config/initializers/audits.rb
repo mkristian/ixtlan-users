db0656f86ac67a5408bd327a3036bd74ce8df6b5
Christian Meier <m.kristian@web.de>
2013-01-27 20:56:00 +0530

    fixed typo

--- config/initializers/audits.rb
+++ config/initializers/audits.rb
@@ -1,4 +1,4 @@
 Users::Application.config.audit_manager.block = Proc.new do |manager|
-  manager.keep_log = Configuration.instance.audits_keep_logs
+  manager.keep_logs = Configuration.instance.audits_keep_logs
 end
 Users::Application.config.audit_manager.model = Audit
