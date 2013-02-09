c4816574312d908dafe80bb8becda877b0e42cab
Christian Meier <m.kristian@web.de>
2013-02-09 19:57:27 +0530

    fix vulernable regexp

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -4,7 +4,7 @@ class Domain < ActiveRecord::Base
 
   belongs_to :modified_by, :class_name => "User"
   validates :modified_by_id, :presence => true
-  validates :name, :presence => true, :format => /^[a-z0-9\.]+$/, :length => { :maximum => 32 }
+  validates :name, :presence => true, :format => /\A[A-Za-z0-9\.]+\z/, :length => { :maximum => 32 }
 
   def self.all_changed_after(from)
     unless from.blank?
